ed60cbe4dcfe16de5303e6a267efbc723e530c3f
okok02110211 <okok02110211@gmail.com>
2026-01-05 16:51:23 +0900

    알림 엔티티 생성

---
+++ src/main/java/com/teamgold/goldenharvest/domain/notification/command/domain/aggregate/UserNotification.java
@@ -0,0 +1,45 @@
+package com.teamgold.goldenharvest.domain.notification.command.domain.aggregate;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.apache.catalina.User;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_user_notification")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class UserNotification {
+
+    @Id
+    @Column(name = "user_notification_id", nullable = false, length = 255)
+    private Long user_notification_id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "notification_id", nullable = false)
+    private Notification notification;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "is_read",nullable = false)
+    private Boolean isRead;
+
+    @Column(name = "read_at",nullable = true)
+    private LocalDateTime readAt;
+
+    @CreationTimestamp
+    @Column(name = "recieved_at")
+    private LocalDateTime recievedAt;
+
+    public void markAsRead(LocalDateTime now) {
+        this.isRead = true;
+        this.readAt = now;
+    }
+
+}
