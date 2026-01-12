01b5d5bd585137b36795c273527717d95ad23873
okok02110211 <okok02110211@gmail.com>
2026-01-12 12:21:59 +0900

    발주 기능

---
+++ src/main/java/com/teamgold/goldenharvest/domain/purchases/command/application/event/PurchaseOrderCreatedEvent.java
@@ -0,0 +1,20 @@
+package com.teamgold.goldenharvest.domain.purchases.command.application.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
+@AllArgsConstructor
+public class PurchaseOrderCreatedEvent extends BaseDomainEvent{
+
+    private final String purchaseOrderId;
+    private final LocalDate createdAt;
+    private final int quantity;
+
+    @Override
+    public String type(){
+        return "PurchaseOrderCreated";
+    }
+}
--- src/main/java/com/teamgold/goldenharvest/domain/purchases/command/infrastructure/repository/JpaPurchaseOrderRepository.java
+++ src/main/java/com/teamgold/goldenharvest/domain/purchases/command/infrastructure/repository/JpaPurchaseOrderRepository.java
@@ -1,8 +1,8 @@
 package com.teamgold.goldenharvest.domain.purchases.command.infrastructure.repository;
 
-import com.teamgold.goldenharvest.domain.purchases.command.domain.aggregate.PurchaseOrderItem;
+import com.teamgold.goldenharvest.domain.purchases.command.domain.aggregate.PurchaseOrder;
 import com.teamgold.goldenharvest.domain.purchases.command.domain.repository.PurchaseOrderRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface JpaPurchaseOrderRepository extends PurchaseOrderRepository, JpaRepository<PurchaseOrderItem,Long> {
+public interface JpaPurchaseOrderRepository extends PurchaseOrderRepository, JpaRepository<PurchaseOrder,String> {
 }
