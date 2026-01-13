d4e7328d54da719f558c72eda7cab47836889089
KyujinPark01 <rbwls1100@naver.com>
2026-01-13 15:55:36 +0900

    feat : 예외 기능 처리

---
+++ src/main/java/com/teamgold/goldenharvest/common/exception/BusinessException.java
@@ -0,0 +1,13 @@
+package com.teamgold.goldenharvest.common.exception;
+
+import com.teamgold.goldenharvest.common.exception.ErrorCode;
+import lombok.Getter;
+
+@Getter
+public class BusinessException extends RuntimeException {
+    private final ErrorCode errorCode;
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+}
--- src/main/java/com/teamgold/goldenharvest/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/teamgold/goldenharvest/common/exception/GlobalExceptionHandler.java
@@ -1,10 +1,19 @@
 package com.teamgold.goldenharvest.common.exception;
 
+import com.teamgold.goldenharvest.common.response.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<ApiResponse<?>> handleBusinessException(BusinessException e) {
+        ErrorCode errorCode = e.getErrorCode();
+        log.error("BusinessException: {}: {}", errorCode.getCode(), errorCode.getMessage(), e);
+        return ResponseEntity.status(errorCode.getHttpStatusCode())
+                .body(ApiResponse.fail(errorCode.getCode(), errorCode.getMessage()));
+    }
 }
