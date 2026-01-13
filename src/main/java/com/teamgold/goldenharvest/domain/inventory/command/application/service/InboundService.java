393b7a45d9f79d9abd4aed6a06a9c7d185bf58c3
405supply <405supplychoi@gmail.com>
2026-01-13 17:16:23 +0900

    feat: 입고 데이터 구성 및 transaction 설정

--- src/main/java/com/teamgold/goldenharvest/domain/inventory/command/application/service/InboundService.java
+++ src/main/java/com/teamgold/goldenharvest/domain/inventory/command/application/service/InboundService.java
@@ -1,13 +1,16 @@
 package com.teamgold.goldenharvest.domain.inventory.command.application.service;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.teamgold.goldenharvest.common.idempotent.BloomFilterManager;
 import com.teamgold.goldenharvest.domain.inventory.command.application.dto.PurchaseOrderEvent;
+import com.teamgold.goldenharvest.domain.inventory.command.domain.IdGenerator;
 import com.teamgold.goldenharvest.domain.inventory.command.domain.lot.Inbound;
+import com.teamgold.goldenharvest.domain.inventory.command.infrastructure.IdGeneratorRepository;
 import com.teamgold.goldenharvest.domain.inventory.command.infrastructure.InboundRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -18,9 +21,11 @@ import lombok.RequiredArgsConstructor;
 public class InboundService {
 
 	private final InboundRepository inboundRepository;
+	private final IdGeneratorRepository idGeneratorRepository;
 	private final LotService lotService;
 	private final BloomFilterManager bloomFilterManager;
 
+	@Transactional
 	public String processInbound(PurchaseOrderEvent purchaseOrderEvent) {
 		// idempotency validation
 		String eventType = "purchaseOrderEvent";
@@ -33,11 +38,34 @@ public class InboundService {
 		}
 
 		// Todo: purchaseOrderEvent내 데이터 정합성 검증 로직
-		// Todo: inbound data 구성
-		Inbound inbound = Inbound.builder().build(); // purchaseOrderEvent 데이터를 이용하여 Inbound 생성
+
+		// 입고 ID 생성
+		String inboundNo = createInboundNo();
+
+		// 입고 데이터 구성
+		Inbound inbound = Inbound.builder()
+			.inboundId(inboundNo)
+			.skuNo(purchaseOrderEvent.skuNo())
+			.quantity(purchaseOrderEvent.quantity())
+			.inboundDate(LocalDate.now())
+			.build();
 
 		inboundRepository.save(inbound);
 
 		return lotService.createLot(purchaseOrderEvent);
+	}
+
+	private String createInboundNo() {
+
+		IdGenerator generator = IdGenerator.create();
+		idGeneratorRepository.save(generator);
+
+		Long sequenceNum = generator.getId();
+
+		String today = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+		String formattedSeq = String.format("%06d", sequenceNum);
+
+		// IN_YYYYMMDD_NNNNNN 형식의 native id 생성
+		return "IN_" + today + "_" + formattedSeq;
 	}
 }
