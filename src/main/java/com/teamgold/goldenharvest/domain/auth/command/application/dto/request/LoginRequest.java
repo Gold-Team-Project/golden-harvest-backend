956a26430a3f32fd8764769e7d3990383b350f2f
steamjin <kkjj2024@naver.com>
2026-01-22 22:30:06 +0900

    feat : 회원가입 관련 테스트코드

--- src/main/java/com/teamgold/goldenharvest/domain/auth/command/application/dto/request/LoginRequest.java
+++ src/main/java/com/teamgold/goldenharvest/domain/auth/command/application/dto/request/LoginRequest.java
@@ -2,13 +2,13 @@ package com.teamgold.goldenharvest.domain.auth.command.application.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequest {
     @Email(message = "이메일 형식이 올바르지 않습니다.")
     @NotBlank(message = "이메일은 필수 입력 값입니다.")
--- src/main/java/com/teamgold/goldenharvest/domain/auth/command/application/dto/request/SignUpRequest.java
+++ src/main/java/com/teamgold/goldenharvest/domain/auth/command/application/dto/request/SignUpRequest.java
@@ -1,13 +1,13 @@
 package com.teamgold.goldenharvest.domain.auth.command.application.dto.request;
 
 import jakarta.validation.constraints.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class SignUpRequest {
     @Email(message = "이메일 형식이 올바르지 않습니다.")
     @NotBlank(message = "이메일은 필수 입력 값입니다.")
