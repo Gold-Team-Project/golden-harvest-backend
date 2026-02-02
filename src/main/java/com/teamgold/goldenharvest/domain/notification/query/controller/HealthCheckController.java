9f9ae9f72e4ffb886b49f6b3ab6bb34be90edab5
kang seong hyun <127469438+okok02110211@users.noreply.github.com>
2026-02-02 18:59:27 +0900

    feat: Health check 수정

--- src/main/java/com/teamgold/goldenharvest/domain/notification/query/controller/HealthCheckController.java
+++ src/main/java/com/teamgold/goldenharvest/domain/notification/query/controller/HealthCheckController.java
@@ -2,15 +2,17 @@ package com.teamgold.goldenharvest.domain.notification.query.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@RequestMapping("/api")
 @RestController
 public class HealthCheckController {
 
-    @GetMapping("/")
+    @GetMapping("/health")
     public ResponseEntity<Map<String, String>> healthCheck() {
         Map<String, String> response = new HashMap<>();
         response.put("status", "OK");
