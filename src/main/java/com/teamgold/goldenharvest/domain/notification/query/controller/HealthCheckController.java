a2bffb4a1ec9161b71520b45d6b8589a43c16067
kang seong hyun <127469438+okok02110211@users.noreply.github.com>
2026-02-03 10:44:33 +0900

    deploy: task definition secrete 추가 , healthcheck 주소 변경

--- src/main/java/com/teamgold/goldenharvest/domain/notification/query/controller/HealthCheckController.java
+++ src/main/java/com/teamgold/goldenharvest/domain/notification/query/controller/HealthCheckController.java
@@ -8,15 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 import java.util.Map;
 
-@RequestMapping("/api")
+@RequestMapping("/health")
 @RestController
 public class HealthCheckController {
 
-    @GetMapping("/health")
+    @GetMapping
     public ResponseEntity<Map<String, String>> healthCheck() {
         Map<String, String> response = new HashMap<>();
         response.put("status", "OK");
         response.put("message", "Golden Harvest API is running");
         return ResponseEntity.ok(response);
     }
+
 }
