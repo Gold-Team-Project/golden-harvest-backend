5e5b1d5e5a0588630970f7ac87001ef38cce6bb8
kang seong hyun <127469438+okok02110211@users.noreply.github.com>
2026-02-05 19:33:36 +0900

    redis error 추적

--- src/main/java/com/teamgold/goldenharvest/common/config/RedisConfig.java
+++ src/main/java/com/teamgold/goldenharvest/common/config/RedisConfig.java
@@ -1,26 +1,44 @@
 package com.teamgold.goldenharvest.common.config;
 
+import io.lettuce.core.ClientOptions;
+import io.lettuce.core.TimeoutOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import java.time.Duration;
+
 @Configuration
 public class RedisConfig {
 
-    @Value("${spring.data.redis.host}")
-    private String host;
-
-    @Value("${spring.data.redis.port}")
-    private int port;
-
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        // Lettuce라는 라이브러리를 통해 Redis와 연결합니다.
-        return new LettuceConnectionFactory(host, port);
+    public LettuceConnectionFactory redisConnectionFactory(
+            @Value("${spring.data.redis.host}") String host,
+            @Value("${spring.data.redis.port}") int port
+    ) {
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration(host, port);
+
+        ClientOptions clientOptions = ClientOptions.builder()
+                .timeoutOptions(TimeoutOptions.enabled(Duration.ofSeconds(3)))
+                .autoReconnect(true)
+                .build();
+
+        LettuceClientConfiguration clientConfig =
+                LettuceClientConfiguration.builder()
+                        .useSsl()
+                        .and()
+                        .clientOptions(clientOptions)
+                        .commandTimeout(Duration.ofSeconds(3))
+                        .shutdownTimeout(Duration.ofSeconds(2))
+                        .build();
+
+        return new LettuceConnectionFactory(redisConfig, clientConfig);
     }
 
     @Bean
